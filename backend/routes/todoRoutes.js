384d06feb3bd2596a280628577106a92e57ddc4e
Dishant <dishantd442442@gmail.com>
2024-05-02 18:36:26 +0530

    made some changes in the route file to update todo in the db

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { Todo } = require("../database/db");
 
-router.post("/todo", (req, res) => {
+router.post("/todo", async (req, res) => {
   //logic to create a todo
   const title = req.body.title;
   const description = req.body.description;
   try {
-    const newTodo = Todo.create({
+    const newTodo = await Todo.create({
       title: title,
       description: description,
       completed: false,
@@ -22,23 +22,42 @@ router.post("/todo", (req, res) => {
     });
   }
 });
-router.get("/todos/:todoId", (req, res) => {
-  const todos = req.params.todoId;
+router.get("/todos/:todoId", async (req, res) => {
+  const todoId = req.params.todoId;
 
   try {
-    const getTodo = Todo.findOne({
-      todos,
-    });
+    const getTodo = await Todo.findById(todoId);
 
-    res.json({
-      todos: getTodo,
-    });
+    if (!getTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    res.json({ todo: getTodo });
   } catch (error) {
-    res.json({
-      message: "Error while fetching todos",
-    });
+    console.error("Error while fetching todo:", error);
+    res.status(500).json({ message: "Error while fetching todo" });
   }
 });
+
+router.put("/completed/:todoId", async (req, res) => {
+  const todoId = req.params.todoId;
+  try {
+    const todo = await Todo.findById({
+      todoId,
+    });
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    todo.completed = true;
+    await todo.save();
+
+    res.json({ message: "Todo marked as completed" });
+  } catch (error) {
+    message: "Something went wrong";
+  }
+});
+
 router.get("/completed", async (req, res) => {
   try {
     const completedTodos = await Todo.find({ completed: true });
