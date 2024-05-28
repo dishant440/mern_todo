95c0814b65382d06e8f0aeb915f18c6ea0f369c4
Dishant <dishantd442442@gmail.com>
2024-05-28 23:25:16 +0530

    installed tailwind css

--- Frontend/src/components/Button.jsx
+++ Frontend/src/components/Button.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Button() {
+export default function Button(props) {
   return (
-    <div>Button</div>
+    <div>
+        <button className="">
+          {}
+        </button>
+    </div>
   )
 }
