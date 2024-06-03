d868c2af4a524dd531cf25ba4f11f24d67d1b55a
Dishant <dishantd442442@gmail.com>
2024-06-04 00:16:05 +0530

    added script to run backend

--- backend/database/db.js
+++ backend/database/db.js
@@ -46,4 +46,4 @@ const userSchema = new mongoose.Schema({
 // Create Todo model
 const Todo = mongoose.model("Todo", todoSchema);
 const User = mongoose.model("User", userSchema);
-module.exports = { Todo, ConnectToDB };
+module.exports = { Todo, ConnectToDB,User };
