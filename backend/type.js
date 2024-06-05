448088d331cba622c466150c32ddc7a3285094ed
Dishant <dishantd442442@gmail.com>
2024-06-05 17:05:01 +0530

    made changes in types.js

--- backend/type.js
+++ backend/type.js
@@ -1,13 +1,5 @@
 const zod = require("zod");
 
-const createTodo = zod.object({
-  title: zod.string(),
-  description: zod.string(),
-});
-
-const updateTodo = zod.object({
-  id: zod.string(),
-});
 
 const createUser = zod.object({
   email:zod.string().email(),
@@ -16,7 +8,5 @@ const createUser = zod.object({
 })
 
 module.exports = {
-   createTodo,
-   updateTodo,
-   createUser
+  createUser
 };
