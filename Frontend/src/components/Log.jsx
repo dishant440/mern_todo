95c5a8b96b359aad650d386c6768936350dcd373
Dishant <dishantd442442@gmail.com>
2024-06-07 14:17:16 +0530

    navbar done

--- Frontend/src/components/Log.jsx
+++ Frontend/src/components/Log.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export default function Log() {
+export default function Log(props) {
   return (
-    <div>
-        Log User
+    <div className="flex flex-row  mr-5">
+      <div className="rounded-full h-8 w-8 bg-black text-cyan-50 flex items-center justify-center mx-4">
+        D
+      </div>
+      Dishant
     </div>
-  )
+  );
 }
--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Log from './Log'
+import React from 'react';
+import Log from './Log';
 
 export default function NavBar() {
   return (
-    <div className="flex items-center justify-between  p-4 bg-slate-200 shadow shadow-md">
-      <h1 className="flex-grow text-center text-4xl">My Todo</h1>
-      <div className="flex-shrink-0 mr-4">
-        <Log />
-      </div>
+    <div className="flex items-center justify-between p-4 bg-slate-200 shadow-md">
+        <h1 className="flex-grow text-center text-4xl text-blue-500 font-bold">My Todo</h1>
+        <div className="flex-shrink-0 mr-4">
+            <Log />
+        </div>
     </div>
   )
 }
